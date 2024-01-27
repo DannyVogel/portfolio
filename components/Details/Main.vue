df992fff8d69ba8d8f7b62421bd6f03a13abe06d
dvogelg4al <danny.hj.vogel@gmail.com>
2024-01-27 10:42:52 +0100

    Style: adjust responsiveness

--- components/Details/Main.vue
+++ components/Details/Main.vue
@@ -2,14 +2,15 @@
 
 <template>
   <div class="flex gap-4 items-center">
-    <div class="flex flex-col gap-2 mr-auto">
+    <div class="flex flex-col justify-center gap-2 mr-auto">
       <h1 class="text-3xl">Danny Vogel</h1>
 
       <h1 class="text-xl">{{ $t("title") }}</h1>
 
-      <p class="text-sm">{{ $t("description") }}</p>
+      <p class="hidden sm:block text-sm">{{ $t("description") }}</p>
       <DetailsSocials />
     </div>
     <img src="/img/me.png" alt="Danny Vogel" width="150" />
   </div>
+  <p class="sm:hidden text-sm">{{ $t("description") }}</p>
 </template>
