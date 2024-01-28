dcefa0fa8ef337a0b5b0c83e4c982239b234d139
dvogelg4al <danny.hj.vogel@gmail.com>
2024-01-28 22:31:58 +0100

    Chore: update texts, locales

--- i18n.config.ts
+++ i18n.config.ts
@@ -22,6 +22,23 @@ export default defineI18nConfig(() => ({
       },
       copied: "Copied to clipboard!",
       techAndTools: "Technologies and Tools",
+      workExperience: "Work Experience",
+      jobs: {
+        GFAL: {
+          title: "Frontend Developer",
+          company: "GFAL",
+          date: "September 2021 - Present",
+          description:
+            "Spearheading the frontend development of the company's cutting-edge marketplace website, gfal.market, serving as a hub for a user's account and providing the ability to purchase in-game items using its crypto token. Integrating multiple services, including the ID platform, backend server, and a cryptocurrency exchange rate service, to deliver a user-friendly, responsive, and performant website experience.",
+        },
+        Chingu: {
+          title: "Volunteer Frontend Developer",
+          company: "Chingu",
+          date: "March 2023 - May 2023",
+          description:
+            "Collaborated remotely with an international team to develop responsiveweb applications using agile and scrum methodologies. ",
+        },
+      },
     },
     ES: {
       welcome: "Bienvenido",
@@ -43,6 +60,137 @@ export default defineI18nConfig(() => ({
       },
       copied: "¡Copiado al portapapeles!",
       techAndTools: "Tecnologías y Herramientas",
+      workExperience: "Experiencia Laboral",
+      jobs: {
+        GFAL: {
+          title: "Desarrollador Frontend",
+          company: "GFAL",
+          date: "Septiembre 2021 - Presente",
+          description:
+            "Dirigiendo el desarrollo frontend del sitio web de vanguardia de la empresa, gfal.market, que sirve como centro de la cuenta de un usuario y proporciona la capacidad de comprar artículos de juegos usando su criptomoneda. Integrando múltiples servicios, incluida la plataforma ID, el servidor backend y un servicio de tipo de cambio de criptomonedas, para ofrecer una experiencia de sitio web fácil de usar, responsive y de alto rendimiento.",
+        },
+        Chingu: {
+          title: "Desarrollador Frontend Voluntario",
+          company: "Chingu",
+          date: "Marzo 2023 - Mayo 2023",
+          description:
+            "Colaboró de forma remota con un equipo internacional para desarrollar aplicaciones web responsivas utilizando metodologías ágiles y scrum.",
+        },
+      },
+    },
+    DE: {
+      welcome: "Willkommen",
+      title: "Frontend-Entwickler",
+      description:
+        "Motivierter und fleißiger Profi, der versucht, seine Software-Engineering-Fähigkeiten in Kombination mit allgemeinen Problemlösungsfähigkeiten, Sprachen und einer Leidenschaft für Computer und Technologie als Frontend-Entwickler zu nutzen.",
+      languages: {
+        title: "Sprachen",
+        EN: "Englisch",
+        ES: "Spanisch",
+        DE: "Deutsch",
+        FR: "Französisch",
+        CA: "Katalanisch",
+      },
+      contact: {
+        email: "Kopieren Sie meine E-Mail-Adresse",
+        linkedin: "Zu meinem LinkedIn-Profil gehen",
+        github: "Zu meinem GitHub-Profil gehen",
+      },
+      copied: "In die Zwischenablage kopiert!",
+      techAndTools: "Technologien und Werkzeuge",
+      workExperience: "Berufserfahrung",
+      jobs: {
+        GFAL: {
+          title: "Frontend-Entwickler",
+          company: "GFAL",
+          date: "September 2021 - Heute",
+          description:
+            "Leitet die Frontend-Entwicklung der modernen Marktplatz-Website des Unternehmens, gfal.market, die als Hub für das Konto eines Benutzers dient und die Möglichkeit bietet, In-Game-Artikel mit seiner Kryptowährung zu kaufen. Integration mehrerer Dienste, einschließlich der ID-Plattform, des Backend-Servers und eines Kryptowährungswechselkurses, um eine benutzerfreundliche, responsive und leistungsstarke Website-Erfahrung zu bieten.",
+        },
+        Chingu: {
+          title: "Freiwilliger Frontend-Entwickler",
+          company: "Chingu",
+          date: "März 2023 - Mai 2023",
+          description:
+            "Zusammenarbeit mit einem internationalen Team, um responsive Webanwendungen unter Verwendung agiler und Scrum-Methoden zu entwickeln.",
+        },
+      },
+    },
+    FR: {
+      welcome: "Bienvenue",
+      title: "Développeur Frontend",
+      description:
+        "Professionnel motivé et travailleur cherchant à tirer parti de ses compétences en génie logiciel, en combinaison avec des compétences générales en résolution de problèmes, des langues et une passion pour l'informatique et la technologie, en tant que développeur frontend.",
+      languages: {
+        title: "Langues",
+        EN: "Anglais",
+        ES: "Espagnol",
+        DE: "Allemand",
+        FR: "Français",
+        CA: "Catalan",
+      },
+      contact: {
+        email: "Copier mon adresse e-mail",
+        linkedin: "Aller à mon profil LinkedIn",
+        github: "Aller à mon profil GitHub",
+      },
+      copied: "Copié dans le presse-papiers!",
+      techAndTools: "Technologies et outils",
+      workExperience: "Expérience professionnelle",
+      jobs: {
+        GFAL: {
+          title: "Développeur Frontend",
+          company: "GFAL",
+          date: "Septembre 2021 - Aujourd'hui",
+          description:
+            "Diriger le développement frontend du site Web de pointe de l'entreprise, gfal.market, servant de hub pour le compte d'un utilisateur et offrant la possibilité d'acheter des articles de jeu à l'aide de sa crypto-monnaie. Intégration de plusieurs services, dont la plate-forme ID, le serveur backend et un service de taux de change de crypto-monnaie, pour offrir une expérience de site Web conviviale, responsive et performante.",
+        },
+        Chingu: {
+          title: "Développeur Frontend bénévole",
+          company: "Chingu",
+          date: "Mars 2023 - Mai 2023",
+          description:
+            "Collaboration à distance avec une équipe internationale pour développer des applications Web responsive en utilisant des méthodologies agiles et Scrum.",
+        },
+      },
+    },
+    CA: {
+      welcome: "Benvingut",
+      title: "Desenvolupador Frontend",
+      description:
+        "Professional motivat i treballador que busca aprofitar les seves habilitats d'enginyeria de programari, en combinació amb habilitats generals de resolució de problemes, idiomes i passió per la informàtica i la tecnologia, com a desenvolupador frontend.",
+      languages: {
+        title: "Idiomes",
+        EN: "Anglès",
+        ES: "Espanyol",
+        DE: "Alemany",
+        FR: "Francès",
+        CA: "Català",
+      },
+      contact: {
+        email: "Copiar la meva adreça de correu electrònic",
+        linkedin: "Anar al meu perfil de LinkedIn",
+        github: "Anar al meu perfil de GitHub",
+      },
+      copied: "Copiat al porta-retalls!",
+      techAndTools: "Tecnologies i eines",
+      workExperience: "Experiència laboral",
+      jobs: {
+        GFAL: {
+          title: "Desenvolupador Frontend",
+          company: "GFAL",
+          date: "Setembre 2021 - Avui",
+          description:
+            "Dirigint el desenvolupament frontend del lloc web de vanguardia de l'empresa, gfal.market, que serveix com a centre del compte d'un usuari i proporciona la capacitat de comprar articles de jocs utilitzant la seva criptomoneda. Integrant múltiples serveis, incloent la plataforma ID, el servidor backend i un servei de tipus de canvi de criptomonedes, per oferir una experiència de lloc web fàcil d'utilitzar, responsive i de gran rendiment.",
+        },
+        Chingu: {
+          title: "Desenvolupador Frontend voluntari",
+          company: "Chingu",
+          date: "Març 2023 - Maig 2023",
+          description:
+            "Col·laboració a distància amb un equip internacional per desenvolupar aplicacions web responsive utilitzant metodologies àgils i Scrum.",
+        },
+      },
     },
   },
 }));
