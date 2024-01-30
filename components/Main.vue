172ab5c67c3d0a93c538102d3fe5c97f41570f93
dvogelg4al <danny.hj.vogel@gmail.com>
2024-01-30 12:11:01 +0100

    Style: increase gap between sections

--- components/Main.vue
+++ components/Main.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="mt-4 flex flex-col gap-4">
+  <div class="mt-4 flex flex-col gap-6">
     <DetailsMain />
     <SkillsMain />
     <WorkMain />
