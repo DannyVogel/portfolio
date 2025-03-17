f3ea9bb5d8b14ca7f55018a50a73511ba288dc29
Danny <danny.hj.vogel@gmail.com>
2025-03-17 22:45:16 +0100

    Chore: update projects

--- components/Projects/Main.vue
+++ components/Projects/Main.vue
@@ -1,6 +1,30 @@
 <script lang="ts" setup>
 const projects = [
   {
+    title: "InvictoAI",
+    link: "https://invicto.netlify.app/",
+    description: "projectsList.InvictoAI.description",
+    logo: "/img/invicto.png",
+    techStack: {
+      Nuxt: "i-logos-nuxt-icon",
+      TypeScript: "i-logos-typescript-icon",
+      TailwindCSS: "i-logos-tailwindcss-icon",
+      Supabase: "i-logos-supabase-icon",
+    },
+  },
+  {
+    title: "gameState",
+    link: "https://dv-gamestate.netlify.app/",
+    description: "projectsList.gameState.description",
+    logo: "/img/gameState.png",
+    techStack: {
+      Nuxt: "i-logos-nuxt-icon",
+      TypeScript: "i-logos-typescript-icon",
+      TailwindCSS: "i-logos-tailwindcss-icon",
+      Firebase: "i-logos-firebase",
+    },
+  },
+  {
     title: "SignWise",
     link: "https://www.youtube.com/watch?v=Gznc_BwetM4&ab_channel=SignWiseTeam",
     description: "projectsList.SignWise.description",
@@ -24,17 +48,6 @@ const projects = [
     },
   },
   {
-    title: "gameState",
-    link: "https://dv-gamestate.netlify.app/",
-    description: "projectsList.gameState.description",
-    logo: "/img/gameState.png",
-    techStack: {
-      React: "i-logos-react",
-      TypeScript: "i-logos-typescript-icon",
-      Firebase: "i-logos-firebase",
-    },
-  },
-  {
     title: "ToDo",
     link: "https://todo-2f3e3.web.app/",
     description: "projectsList.toDo.description",
@@ -45,17 +58,6 @@ const projects = [
       Firebase: "i-logos-firebase",
     },
   },
-  {
-    title: "Rock Paper Scissors",
-    link: "https://rock-paper-scissors-2f3e3.web.app/",
-    description: "projectsList.rockPaperScissors.description",
-    logo: "/img/rockPaperScissors.png",
-    techStack: {
-      TypeScript: "i-logos-typescript-icon",
-      HTML: "i-logos-html-5",
-      CSS: "i-logos-css-3",
-    },
-  },
 ];
 </script>
 
@@ -63,14 +65,20 @@ const projects = [
   <div>
     <h1 class="text-xl font-bold">{{ $t("projects") }}</h1>
     <div class="mt-4 flex flex-col gap-6">
-      <template v-for="project in projects" :key="project.title">
+      <template
+        v-for="project in projects"
+        :key="project.title"
+      >
         <div class="flex items-start gap-2">
           <a
             :href="project.link"
             target="_blank"
             class="hover:scale-110 transform transition-transform duration-300 w-8 h-8 rounded-full flex-shrink-0 flex items-start justify-center"
           >
-            <img :src="project.logo" alt="logo" class=""
+            <img
+              :src="project.logo"
+              alt="logo"
+              class=""
           /></a>
           <div class="flex flex-col justify-start gap-1">
             <h1 class="-mt-1 leading-none">
@@ -83,7 +91,10 @@ const projects = [
             </h1>
             <p class="text-sm text-gray-400">{{ $t(project.description) }}</p>
             <div class="flex gap-2">
-              <template v-for="(icon, key) in project.techStack" :key="key">
+              <template
+                v-for="(icon, key) in project.techStack"
+                :key="key"
+              >
                 <UTooltip :text="key">
                   <TechIcon :name="icon" />
                 </UTooltip>
