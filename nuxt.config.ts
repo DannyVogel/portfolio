f76c28892824288953f11d5c8776a4c12f1f4e89
Danny <danny.hj.vogel@gmail.com>
2025-03-17 21:13:24 +0100

    Chore: update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,36 +4,38 @@ import { getIconCollections } from "@egoist/tailwindcss-icons";
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
+
   modules: [
     "@nuxt/ui",
     "@nuxtjs/i18n",
     "@formkit/auto-animate",
     "@nuxtjs/google-fonts",
   ],
-  tailwindcss: {
-    viewer: false,
-  },
+
   colorMode: {
     preference: "dark",
     fallback: "dark",
   },
+
+  // ui: {
+  //   icons: {
+  //     extraProperties: { "mask-size": "contain", "mask-position": "center" },
+  //     collections: {
+  //       ...getIconCollections([
+  //         "heroicons",
+  //         "ph",
+  //         "logos",
+  //         "noto-v1",
+  //         "eos-icons",
+  //       ]),
+  //     },
+  //   },
+  // },
   googleFonts: {
     families: {
       Kanit: true,
     },
   },
-  ui: {
-    icons: {
-      extraProperties: { "mask-size": "contain", "mask-position": "center" },
-      collections: {
-        ...getIconCollections([
-          "heroicons",
-          "ph",
-          "logos",
-          "noto-v1",
-          "eos-icons",
-        ]),
-      },
-    },
-  },
+
+  compatibilityDate: "2025-03-17",
 });
