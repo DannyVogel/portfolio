ec221720e9d32e2a2355e8d7d08b9c99b7ff2d54
Danny <danny.hj.vogel@gmail.com>
2025-03-28 00:05:50 +0100

    Style: update

--- layouts/garden.vue
+++ layouts/garden.vue
@@ -1,11 +1,11 @@
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <div class="px-4 md:px-20 sm:grid sm:grid-cols-[200px_1fr_200px] gap-12 max-h-[calc(100vh-1rem)] overflow-y-scroll">
+  <div
+    class="px-4 md:px-20 sm:grid sm:grid-cols-[200px_1fr_200px] gap-12 max-h-[calc(100vh-1rem)] overflow-y-auto"
+  >
     <GardenSidebar />
-    <div class="pt-10 max-w-screen overflow-x-hidden">
+    <div class="pt-10 prose dark:prose-invert min-w-full">
       <slot />
     </div>
     <GardenGraph />
