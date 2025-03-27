0e96f2fe53d6586260f81429990bf09abda62bef
Danny <danny.hj.vogel@gmail.com>
2025-03-28 00:05:45 +0100

    Style: update

--- components/Garden/Sidebar.vue
+++ components/Garden/Sidebar.vue
@@ -1,9 +1,13 @@
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <div class="sticky top-0 max-h-[calc(100vh-1rem)] overflow-y-auto mb-10 sm:mb-0 pt-4">
-    <h1 class="text-2xl font-bold italic">Digital Garden</h1>
+  <div class="sticky top-0 max-h-[calc(100vh-1rem)] mb-10 sm:mb-0 pt-4">
+    <NuxtLink to="/garden">
+      <h1
+        class="text-2xl font-bold italic hover:scale-105 hover:text-blue-500 transition-all duration-300"
+      >
+        Digital Garden
+      </h1>
+    </NuxtLink>
   </div>
 </template>
