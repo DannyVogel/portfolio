7e85e9c4c523a804124fdbd507ac2e88c95d394d
Danny <danny.hj.vogel@gmail.com>
2025-03-31 22:23:54 +0200

    Enhance: add SEO metadata for improved page visibility

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Danny Vogel",
+  description: "Frontend Developer",
+  ogTitle: "Danny Vogel",
+  ogDescription: "Frontend Developer",
+});
+</script>
+
 <template>
   <NuxtLayout>
     <Main />
