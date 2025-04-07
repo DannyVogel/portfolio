fe0fed9afb9ee0a1436c79292c2a6e9b2084f64a
Danny <danny.hj.vogel@gmail.com>
2025-04-07 22:41:13 +0200

    Fix: limit sticky to non-mobile views

--- components/Garden/Sidebar.vue
+++ components/Garden/Sidebar.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="sticky top-0 max-h-[calc(100vh-1rem)] mb-10 sm:mb-0 pt-4">
+  <div class="sm:sticky sm:top-0 max-h-[calc(100vh-1rem)] mb-10 sm:mb-0 pt-4">
     <NuxtLink to="/garden">
       <h1
         class="text-2xl font-bold italic hover:scale-105 hover:text-blue-500 transition-all duration-300"
