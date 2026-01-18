5145e2f9d27800a6571e4a96f50dc4fb76489279
Danny Vogel <kelevra>
2026-01-18 23:14:43 +0200

    Fix: update Cloudflare preset in Nuxt configuration from 'cloudflare_module' to 'cloudflare_pages' for improved deployment compatibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
     "@nuxt/content",
   ],
   nitro: {
-    preset: "cloudflare_module",
+    preset: "cloudflare_pages",
     cloudflare: {
       deployConfig: true,
       nodeCompat: true
